91d168d1ebec8925a906a9732fbc955fa5b1e999
katomasahiro10 <katomasahiro10@gmail.com>
2021-09-15 02:38:20 +0900

    Rearrange folders

---
+++ myexamples/test_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+python neat_example.py > /dev/null
+if [ $? -ne 0 ]; then
+    echo "=========================================="
+    echo " 😡 Oops... run neat_example.py FAILURED"
+    echo "=========================================="
+    exit 1
+fi
+echo "😁 neat_example.py is OK"
+
+python exneat_example.py > /dev/null
+if [ $? -ne 0 ]; then
+    echo "=========================================="
+    echo " 😡 Oops... run exneat_example.py FAILURED"
+    echo "=========================================="
+    exit 1
+fi
+echo "😁 exneat_example.py is OK"
+
+python modneat_example.py > /dev/null
+if [ $? -ne 0 ]; then
+    echo "=========================================="
+    echo " 😡 Oops... run modneat_example.py FAILURED"
+    echo "=========================================="
+    exit 1
+fi
+echo "😁 modneat_example.py is OK"
+
+python exmod_example.py > /dev/null
+if [ $? -ne 0 ]; then
+    echo "=========================================="
+    echo " 😡 Oops... run exmod_example.py FAILURED"
+    echo "=========================================="
+    exit 1
+fi
+echo "😁 exmod_example.py is OK"
+
+echo "======================================="
+echo " 😁😁😁 ALL TESTS ARE PASSED !!  "
+echo "======================================="
