47e31d471b324c6fdfa70f4efe83d6b490b7e2b4
katomasahiro10 <katomasahiro10@gmail.com>
2021-09-14 18:57:08 +0900

    add ExModFeedForwardNetwork in __init__ file

--- modneat/nn/__init__.py
+++ modneat/nn/__init__.py
@@ -1,3 +1,4 @@
 from modneat.nn.feed_forward import FeedForwardNetwork
 from modneat.nn.mod_feed_forward import ModFeedForwardNetwork
+from modneat.nn.ex_mod_feed_forward import ExModFeedForwardNetwork
 from modneat.nn.recurrent import RecurrentNetwork
