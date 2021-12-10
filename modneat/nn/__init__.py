9af048ad8e0e69fc69c89718cce11ffc1c22a7b1
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-10 14:21:50 +0900

    update: resgist new model

--- modneat/nn/__init__.py
+++ modneat/nn/__init__.py
@@ -3,3 +3,4 @@ from modneat.nn.mod_feed_forward import ModFeedForwardNetwork
 from modneat.nn.ex_mod_feed_forward import ExModFeedForwardNetwork
 from modneat.nn.ex_feed_forward import ExFeedForwardNetwork
 from modneat.nn.recurrent import RecurrentNetwork
+from modneat.nn.hebbian_ffn import HebbianFNN
