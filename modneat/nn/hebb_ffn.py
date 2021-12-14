02a2c88c6c8dd194e116cd40d1b56ad44995e2ac
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-15 06:28:19 +0900

    update: HebbFNN
    HebbFNN Classが動くように

--- modneat/nn/hebb_ffn.py
+++ modneat/nn/hebb_ffn.py
@@ -22,7 +22,30 @@ class HebbFNN(object):
             s = agg_func(node_inputs)
             self.values[node] = act_func(bias + response * s)
 
+        for node, act_func, agg_func, bias, response, links in self.node_evals:
+            node_inputs = []
+            for i, w in links:
+                update_val =  0.1 * self.values[i] * self.values[node]
+                self.weight_change(i, node, update_val)
+
         return [self.values[i] for i in self.output_nodes]
+
+    def weight_change(self, input_node, output_node, value):
+        """ Add value to connection weight between input_node and output_node. """
+        node_loop_counter = -1
+
+        for _output_node, act_func, agg_func, bias, response, links in self.node_evals:
+            node_loop_counter += 1
+            connection_loop_counter = -1
+            for _input_node, _weight in links:
+                connection_loop_counter += 1
+                if(input_node == _input_node and output_node == _output_node):
+                    listed_node_and_weight = list(self.node_evals[node_loop_counter][5][connection_loop_counter])
+                    listed_node_and_weight[1] += value
+                    self.node_evals[node_loop_counter][5][connection_loop_counter] = tuple(listed_node_and_weight)
+                    #print('ノード{},{}の間の重みを {} だけ変更しました'.format(input_node, output_node, value))
+                else:
+                    pass
 
     @staticmethod
     def create(genome, config):
