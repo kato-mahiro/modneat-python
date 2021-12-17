03e1dfc662797e32515c1f4f586aec12042d5ac5
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-18 02:36:46 +0900

    update: run.sh

--- myexamples/run.sh
+++ myexamples/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -euo pipefail
-ln -sf ../utils/parse_options.sh ./parse_options.sh
 
 model='FeedForwardNetwork'
 config='./configs/default_genome.ini'
 task='task.xor'
 job_no=1
 generation=10
+savedir='./results'
 
 help_message=$(cat << EOF
 Usage: $0 [options]
@@ -18,13 +18,9 @@ Options:
     --task: モデルに課すタスクを指定 (default=${task})
     --job_no: 並列実行数を指定 (default=${task})
     --generation: 実験を何世代行うかを指定 (default=${task})
-
+    --savedir: 実験結果を保存するディレクトリ (default=${savedir})
 EOF
 )
 
 . ../utils/parse_options.sh
 
-#required args
-model=$1
-
-echo $job_no
