812179d2c2a47d46123ef0d200bcd6ffd8cc8e21
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-18 05:57:07 +0900

    update: change arg name
    configという名前だと変な挙動(指定したパスのフォルダを1つづつ実行始める)

--- myexamples/run.sh
+++ myexamples/run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 network='FeedForwardNetwork'
-config='./configs/default_genome.ini'
+config_file='./configs/default_genome.ini'
 task='task.xor'
 job_no=1
 generation=10
@@ -15,7 +15,7 @@ Usage: $0 [options]
 
 Options:
     --network: 実験に用いるネットワークの種類を指定 (default=${network})
-    --config: モデル用の設定ファイルを指定 (default=${config})
+    --config_file: モデル用の設定ファイルを指定 (default=${config_file})
     --task: モデルに課すタスクを指定 (default=${task})
     --job_no: 並列実行数を指定 (default=${task})
     --generation: 実験を何世代行うかを指定 (default=${task})
@@ -24,13 +24,16 @@ EOF
 )
 
 . ../utils/parse_options.sh
+echo $network
+echo $config_file
 
 savesubdir=$(echo "${task}_${network}"  | tr '.' '_')
+
 
 ../utils/run.pl JOB_NO=1:${job_no} ${logdir}/log.JOB_NO.txt \
     python ./example.py \
     --network $network \
-    --config $config \
+    --config $config_file \
     --task $task \
     --generation $generation\
     --savedir $savedir/$savesubdir \
