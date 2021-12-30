a3c335d3b56020fefa9d45e61efa1ff10c3055f9
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-30 19:26:10 +0900

    update: enable weight_change both link has 2 or 6 items

--- modneat/nn/utils.py
+++ modneat/nn/utils.py
@@ -5,12 +5,25 @@ def weight_change(self, input_node, output_node, value):
     for _output_node, act_func, agg_func, bias, response, links in self.node_evals:
         node_loop_counter += 1
         connection_loop_counter = -1
-        for _input_node, _weight in links:
-            connection_loop_counter += 1
-            if(input_node == _input_node and output_node == _output_node):
-                listed_node_and_weight = list(self.node_evals[node_loop_counter][5][connection_loop_counter])
-                listed_node_and_weight[1] += value
-                self.node_evals[node_loop_counter][5][connection_loop_counter] = tuple(listed_node_and_weight)
-                #print('ノード{},{}の間の重みを {} だけ変更しました'.format(input_node, output_node, value))
-            else:
-                pass
+
+        if(len(links) == 2):
+            for _input_node, _ in links:
+                connection_loop_counter += 1
+                if(input_node == _input_node and output_node == _output_node):
+                    listed_node_and_weight = list(self.node_evals[node_loop_counter][5][connection_loop_counter])
+                    listed_node_and_weight[1] += value
+                    self.node_evals[node_loop_counter][5][connection_loop_counter] = tuple(listed_node_and_weight)
+                    #print('ノード{},{}の間の重みを {} だけ変更しました'.format(input_node, output_node, value))
+                else:
+                    pass
+
+        elif(len(links) == 6):
+            for _input_node, _, _, _, _, _ in links:
+                connection_loop_counter += 1
+                if(input_node == _input_node and output_node == _output_node):
+                    listed_node_and_weight = list(self.node_evals[node_loop_counter][6][connection_loop_counter])
+                    listed_node_and_weight[1] += value
+                    self.node_evals[node_loop_counter][6][connection_loop_counter] = tuple(listed_node_and_weight)
+                    #print('ノード{},{}の間の重みを {} だけ変更しました'.format(input_node, output_node, value))
+                else:
+                    pass
