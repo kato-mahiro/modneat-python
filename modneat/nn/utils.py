ec5081f19d793d23ae5a1b61030c90a31a9fc87e
katomasahiro10 <katomasahiro10@gmail.com>
2021-12-31 12:53:04 +0900

    hotfix: specify correct element of node_evals
    inputsの中に変更対象の値が入っている
    inputsはタプルの末尾なので、-1を指定

--- modneat/nn/utils.py
+++ modneat/nn/utils.py
@@ -12,9 +12,9 @@ def weight_change(self, input_node, output_node, value):
             for _input_node, _ in links:
                 connection_loop_counter += 1
                 if(input_node == _input_node and output_node == _output_node):
-                    listed_node_and_weight = list(self.node_evals[node_loop_counter][5][connection_loop_counter])
+                    listed_node_and_weight = list(self.node_evals[node_loop_counter][-1][connection_loop_counter])
                     listed_node_and_weight[1] += value
-                    self.node_evals[node_loop_counter][5][connection_loop_counter] = tuple(listed_node_and_weight)
+                    self.node_evals[node_loop_counter][-1][connection_loop_counter] = tuple(listed_node_and_weight)
                     #print('ノード{},{}の間の重みを {} だけ変更しました'.format(input_node, output_node, value))
                 else:
                     pass
@@ -23,9 +23,9 @@ def weight_change(self, input_node, output_node, value):
             for _input_node, _, _, _, _, _ in links:
                 connection_loop_counter += 1
                 if(input_node == _input_node and output_node == _output_node):
-                    listed_node_and_weight = list(self.node_evals[node_loop_counter][6][connection_loop_counter])
+                    listed_node_and_weight = list(self.node_evals[node_loop_counter][-1][connection_loop_counter])
                     listed_node_and_weight[1] += value
-                    self.node_evals[node_loop_counter][6][connection_loop_counter] = tuple(listed_node_and_weight)
+                    self.node_evals[node_loop_counter][-1][connection_loop_counter] = tuple(listed_node_and_weight)
                     #print('ノード{},{}の間の重みを {} だけ変更しました'.format(input_node, output_node, value))
                 else:
                     pass
