ab2fbd85a031cafeb4f6205322fd0abc88a01942
katomasahiro10 <katomasahiro10@gmail.com>
2022-01-11 20:04:17 +0900

    implement new class ExHebbRNN
    拡張ヘブ則で重み更新のRNNを実装
    
    Squashed commit of the following:
    
    commit 4128613a2c1493bb827c228b086d2fed510540ad
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Jan 11 20:03:28 2022 +0900
    
        add config file
    
    commit 78290b0e16eb1070a55261a661566d654cd05c31
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Jan 11 20:02:48 2022 +0900
    
        add: new model ExHebbRNN
    
    commit 067c969f198be68b097431054a107e7fccce2454
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Tue Jan 11 19:41:08 2022 +0900
    
        add: script for test update_change

--- modneat/nn/__init__.py
+++ modneat/nn/__init__.py
@@ -4,6 +4,7 @@ from modneat.nn.mod_hebb_ffn import ModHebbFFN
 from modneat.nn.index_hebb_ffn import IndExHebbFFN
 from modneat.nn.mod_index_hebb_ffn import ModIndExHebbFFN
 from modneat.nn.ex_hebb_ffn import ExHebbFFN
-from modneat.nn.recurrent import RecurrentNetwork
 from modneat.nn.mod_ex_hebb_ffn import ModExHebbFFN
+from modneat.nn.recurrent import RecurrentNetwork
+from modneat.nn.ex_hebb_rnn import ExHebbRNN
 from modneat.nn import utils
