ac34538bc7f85f1ef316c7b55b5578e795a98c4f
katomasahiro10 <katomasahiro10@gmail.com>
2022-03-28 16:34:46 +0900

    add: CartPole-v0 task

--- modneat-examples/task.py
+++ modneat-examples/task.py
@@ -2,6 +2,7 @@ from matplotlib.pyplot import hist
 from modneat import visualize
 import os
 import copy
+import gym
 
 class xor:
     # The XOR inputs and expected corresponding outputs for fitness evaluation
@@ -75,3 +76,48 @@ class non_static(xor):
         self.network_type = network_type
         self.xor_inputs  = [(1.0, 1.0), (1.0, 1.0), (1.0, 1.0), (1.0, 1.0)]
         self.xor_outputs = [   (1.0,),     (0.33,),     (-0.33,),     (-1.0,)]
+
+# This class show the usage of env.gym for modneat
+class cartpole_v0:
+    def __init__(self, network_type):
+        self.env = gym.make('CartPole-v0')
+        self.network_type = network_type
+
+    def eval_fitness(self,net):
+        fitness = 0.0
+        net.reset()
+        observation = self.env.reset() #range of the observation is 4.
+        for step in range(200):
+            action = 0 if net.activate(observation)[0] <= 0.5 else 1
+            observation, reward, done, info = self.env.step(action)
+            fitness += reward
+            if done == True:
+                break
+        return fitness, 0
+
+    def eval_genomes(self, genomes, config):
+        for genome_id, genome in genomes:
+            net = self.network_type.create(genome, config)
+            genome.fitness, genome.history = self.eval_fitness(net) 
+    
+    def show_results(self, best_genome, config, stats, out_dir):
+        # Display the best genome among generations.
+        print('\n ************************* Finish evolution *************************  \n')
+        print('\nBest genome:\n{!s}'.format(best_genome))
+
+        # Show output of the most fit genome against training data.
+        print('\nOutput:')
+        net = self.network_type.create(best_genome, config)
+
+        # Check if the best genome is an adequate XOR solver
+        best_genome_fitness, best_genome_history = self.eval_fitness(net)
+        if best_genome_fitness > config.fitness_threshold:
+            print("\n\nSUCCESS: The XOR problem solver found!!!")
+        else:
+            print("\n\nFAILURE: Failed to find XOR problem solver!!!")
+
+        # Visualize the experiment results
+        node_names = {-1:'Cart Position', -2: 'Cart Velocity', -3: 'Pole Angle', -4:'Pole Velocity At Tip', 0:'output'}
+        visualize.draw_net(config, best_genome, False, node_names=node_names, directory=out_dir)
+        visualize.plot_stats(stats, ylog=False, view=False, filename=os.path.join(out_dir, 'avg_fitness.png'))
+        visualize.plot_species(stats, view=False, filename=os.path.join(out_dir, 'speciation.png'))
