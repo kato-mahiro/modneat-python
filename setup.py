1719bb73398f0bd1de50e949a4e5ae944a4004af
kato-mahiro <katomasahiro10@gmail.com>
2023-10-11 23:40:59 +0900

    0.1.1 (2023の実験で利用する用の設定)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='modneat-python',
-    version='0.1.0',
+    version='0.1.1',
     author='kato-mahiro, cesar.gomes, mirrorballu2',
     author_email='katomasahiro10@gmail.com',
     maintainer='kato-mahiro',
